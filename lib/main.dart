1d077704ade863378480dd7adfb62ed1c66412c6
ayehappy8 <sebaaraus@gmail.com>
2023-12-02 23:00:56 -0300

    Implementacion de imagenes y de navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,132 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+List<String> titulos = <String>[
+  'Registro',
+  'Pensamiento',
+  'Cápsulas',
+  'Perfil'
+];
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const UMind());
 
-  // This widget is the root of your application.
+class UMind extends StatelessWidget {
+  const UMind({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.teal,
+          accentColor: const Color.fromARGB(255, 154, 208, 194),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Inicio(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Inicio extends StatelessWidget {
+  const Inicio({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+      length: titulos.length,
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 154, 208, 194),
+        appBar: MyCustomAppBar(
+          height: 300,
+          appBar: AppBar(
+            flexibleSpace: Column(
+              children: [
+                SizedBox(
+                  height: 250.0,
+                  width: 500.0,
+                  child: Image.asset(
+                    'assets/logo.png',
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            backgroundColor: Colors.transparent,
+          ),
+          bottomWidget: Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.teal,
             ),
-          ],
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  'Registro',
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text('Pensamiento', style: TextStyle(color: Colors.white)),
+                Text('Cápsulas', style: TextStyle(color: Colors.white)),
+                Text('Perfil', style: TextStyle(color: Colors.white)),
+              ],
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 250.0,
+                width: 350.0,
+                child: Image.asset('assets/mascota.png'),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class MyCustomAppBar extends StatelessWidget implements PreferredSize {
+  final AppBar appBar;
+  final Widget bottomWidget;
+  final double height;
+
+  const MyCustomAppBar({
+    Key? key,
+    required this.appBar,
+    required this.bottomWidget,
+    required this.height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: Stack(
+        children: [
+          Container(
+            color: const Color.fromARGB(255, 154, 208, 194),
+            height: height / 1.2,
+            child: appBar,
+          ),
+          Positioned(
+            left: 30,
+            right: 30,
+            bottom: 20,
+            child: Material(
+              //elevation: 2,
+              child: bottomWidget,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget get child => this;
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
 }
