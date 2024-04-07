94b901de414f7efd609ff981e176357e09432b41
ayehappy8 <sebaaraus@gmail.com>
2024-04-07 00:59:34 -0400

    Implementacion de Texto imagenes y dropdownbutton y textfield

--- lib/screens/pensamiento.dart
+++ lib/screens/pensamiento.dart
@@ -1,18 +1,102 @@
 import 'package:flutter/material.dart';
 
-class Pensamiento extends StatelessWidget {
-  const Pensamiento({super.key});
+class Pensamiento extends StatefulWidget {
+  const Pensamiento({Key? key}) : super(key: key);
+
+  @override
+  _PensamientoState createState() => _PensamientoState();
+}
+
+String _emocion = 'Felicidad';
+
+class _PensamientoState extends State<Pensamiento> {
+  void dropdownCallback(String? selectedValue) {
+    if (selectedValue is String) {
+      setState(() {
+        _emocion = selectedValue;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 187, 222, 202),
       body: Container(
-        margin: const EdgeInsets.only(bottom: 15.0),
-        child: const Center(
+        margin: const EdgeInsets.only(bottom: 15.0, left: 10, right: 10),
+        child: Center(
           child: SingleChildScrollView(
-            child: Center(
-              child: Text("Pensamiento"),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  SizedBox(
+                    height: 150.0,
+                    width: 150.0,
+                    child: Image.asset('assets/mascota.png'),
+                  ),
+                  const Text(style: TextStyle(fontSize: 20), "¿Qué pienso?"),
+                ]),
+                const SizedBox(
+                  height: 193,
+                  width: 294,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Color(0xFFECF4D6),
+                      filled: true,
+                      border: OutlineInputBorder(),
+                      hintText: '....',
+                    ),
+                  ),
+                ),
+                //row 1
+                Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  SizedBox(
+                    height: 150.0,
+                    width: 150.0,
+                    child: Image.asset('assets/mascota.png'),
+                  ),
+                  const Text(
+                      style: TextStyle(fontSize: 20),
+                      "¿Qué situación me\n hace pensar esto?"),
+                ]),
+                const SizedBox(
+                  height: 193,
+                  width: 294,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Color(0xFFECF4D6),
+                      filled: true,
+                      border: OutlineInputBorder(),
+                      hintText: '....',
+                    ),
+                  ),
+                ),
+                //row 2
+                Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  SizedBox(
+                    height: 150.0,
+                    width: 150.0,
+                    child: Image.asset('assets/mascota.png'),
+                  ),
+                  const Text(style: TextStyle(fontSize: 20), "¿Qué pienso?"),
+                ]),
+                DropdownButton(items: const [
+                  DropdownMenuItem(
+                    value: "Felicidad",
+                    child: Text("Felicidad"),
+                  ),
+                  DropdownMenuItem(
+                    value: "Tristeza",
+                    child: Text("Tristeza"),
+                  ),
+                  DropdownMenuItem(
+                    value: "Ira",
+                    child: Text("Ira"),
+                  ),
+                ], value: _emocion, onChanged: dropdownCallback),
+                //row 3
+              ],
             ),
           ),
         ),
