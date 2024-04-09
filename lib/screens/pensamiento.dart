36570aad858a25e9c0c47ae2108bb7edf3123d0c
Jose Velasquez <jomanuel1201@gmail.com>
2024-04-09 02:07:41 -0400

    registro emocion

--- lib/screens/pensamiento.dart
+++ lib/screens/pensamiento.dart
@@ -1,18 +1,153 @@
 import 'package:flutter/material.dart';
 
-class Pensamiento extends StatelessWidget {
-  const Pensamiento({super.key});
+class Pensamiento extends StatefulWidget {
+  const Pensamiento({Key? key}) : super(key: key);
+
+  @override
+  _PensamientoState createState() => _PensamientoState();
+}
+
+String _emocion = 'Felicidad';
+
+class _PensamientoState extends State<Pensamiento> {
+  void dropdownCallback(String? selectedValue) {
+    if (selectedValue is String) {
+      setState(() {
+        _emocion = selectedValue;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 187, 222, 202),
       body: Container(
-        margin: const EdgeInsets.only(bottom: 15.0),
-        child: const Center(
+        margin: const EdgeInsets.only(bottom: 30.0, left: 10, right: 10),
+        child: Center(
           child: SingleChildScrollView(
-            child: Center(
-              child: Text("Pensamiento"),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  SizedBox(
+                    height: 150.0,
+                    width: 150.0,
+                    child: Image.asset('assets/mascota.png'),
+                  ),
+                  const Text(style: TextStyle(fontSize: 20), "¿Qué pienso?"),
+                ]),
+                const SizedBox(
+                  height: 193,
+                  width: 294,
+                  child: TextField(
+                    maxLines: 6,
+                    decoration: InputDecoration(
+                      fillColor: Color(0xFFECF4D6),
+                      filled: true,
+                      border: OutlineInputBorder(),
+                      hintText: '....',
+                    ),
+                  ),
+                ),
+                //row 1
+                Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  SizedBox(
+                    height: 150.0,
+                    width: 150.0,
+                    child: Image.asset('assets/mascota.png'),
+                  ),
+                  const Text(
+                      style: TextStyle(fontSize: 20),
+                      "¿Qué situación me\n hace pensar esto?"),
+                ]),
+                const SizedBox(
+                  height: 193,
+                  width: 294,
+                  child: TextField(
+                    maxLines: 6,
+                    decoration: InputDecoration(
+                      fillColor: Color(0xFFECF4D6),
+                      filled: true,
+                      border: OutlineInputBorder(),
+                      hintText: '....',
+                    ),
+                  ),
+                ),
+                //row 2
+                Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  SizedBox(
+                    height: 150.0,
+                    width: 150.0,
+                    child: Image.asset('assets/mascota.png'),
+                  ),
+                  const Text(
+                      style: TextStyle(fontSize: 20),
+                      "¿Qué emoción me \n genera esto?"),
+                ]),
+                Container(
+                  width: 294,
+                  margin: EdgeInsets.only(bottom: 50),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.teal,
+                  ),
+                  child: DropdownButton(
+                    isExpanded: true,
+                    dropdownColor: Colors.teal,
+                    style: const TextStyle(fontSize: 20, color: Colors.white),
+                    borderRadius: BorderRadius.circular(30.0),
+                    items: const [
+                      DropdownMenuItem(
+                        value: "Felicidad",
+                        child: Center(
+                            child: Text(
+                                "Felicidad")), // Centra el texto horizontalmente
+                      ),
+                      DropdownMenuItem(
+                        value: "Tristeza",
+                        child: Center(
+                            child: Text(
+                                "Tristeza")), // Centra el texto horizontalmente
+                      ),
+                      DropdownMenuItem(
+                        value: "Ira",
+                        child: Center(
+                            child:
+                                Text("Ira")), // Centra el texto horizontalmente
+                      ),
+                    ],
+                    value: _emocion,
+                    onChanged: dropdownCallback,
+                  ),
+                ),
+                //row 3
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue[900],
+                          minimumSize: const Size(146, 70),
+                        ),
+                        child: const Text(
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                            "Pensamientos\npasados"),
+                        onPressed: () => {},
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.teal,
+                          minimumSize: const Size(146, 70),
+                        ),
+                        child: const Text(
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                            "Guardar"),
+                        onPressed: () => {},
+                      ),
+                    ]),
+                //row 4
+              ],
             ),
           ),
         ),
--- lib/screens/registroemocion.dart
+++ lib/screens/registroemocion.dart
@@ -5,43 +5,65 @@ class Registroemocion extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Lista de nombres para los botones
+  final List<String> buttonNames = [
+    'Felicidad',
+    'Tristeza',
+    'Ira',
+    'Miedo',
+    'Asco',
+    'Sorpresa',
+  ];
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 187, 222, 202),
       body: Container(
         margin: const EdgeInsets.only(bottom: 15.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Center(
-              child: Column (
-                children: [
-                  Text('Como te sientes hoy?'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text("¿Qué sientes hoy?", style: TextStyle(fontSize: 25)),
 
-                  SizedBox(
-                    height: 300.0,
-                    width: 350.0,
-                    child: Image.asset('assets/mascota.png'),
+                SizedBox(
+                  height: 300.0,
+                  width: 350.0,
+                  child: Image.asset('assets/mascota.png'),
+                ),
+
+                SizedBox(
+                  height: 400.0,
+                  width: 350.0,
+                  child: GridView.count(
+                    crossAxisCount: 2, // Establece 2 columnas
+                    mainAxisSpacing: 4.0, // Espacio vertical entre los elementos
+                    crossAxisSpacing: 4.0, // Espacio horizontal entre los elementos
+                    childAspectRatio: 3 / 2, // Ajusta la proporción de los elementos
+                    physics: const NeverScrollableScrollPhysics(), // Esto deshabilita el desplazamiento del GridView
+                    children: List.generate(buttonNames.length, (index) {
+                      return Center(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.teal,
+                            minimumSize: const Size(146, 45),
+                          ),
+                          onPressed: () {
+                            // Acción al presionar el botón
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Registroemocion()),
+                            );
+                          },
+                          child: Text(
+                            buttonNames[index],
+                            style: const TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ),
+                      );
+                    }),
                   ),
-
-                  const Row(
-                    children: [
-                      Text('ola'),Text('ola'),
-                    ],
-                  ),
-
-                  const Row(
-                    children: [
-                      Text('ola'),Text('ola'),
-                    ],
-                  ),
-
-                  const Row(
-                    children: [
-                      Text('ola'),Text('ola'),
-                    ],
-                  ),                
-                  
-                ],  
-              )
+                ),
+              ],
             ),
           ),
         ),
