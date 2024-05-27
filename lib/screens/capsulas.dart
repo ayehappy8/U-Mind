6da4c8f9f60b0a161e3fb8be0b91420ad884eb42
ayehappy8 <sebaaraus@gmail.com>
2024-05-27 19:02:12 -0400

    Implementacion de capsulas

--- lib/screens/capsulas.dart
+++ lib/screens/capsulas.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '/widget/card.dart';
 
 class Capsulas extends StatelessWidget {
   const Capsulas({super.key});
@@ -15,213 +16,25 @@ class Capsulas extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  //Row 1 - video 1 -
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          const SizedBox(
-                            width: 180,
-                            height: 80,
-                            child: Text(
-                                style: TextStyle(fontSize: 10),
-                                " ¿Cómo trabajar el bienestar emocional? "),
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromARGB(255, 0, 150, 40),
-                              minimumSize: const Size(107, 21),
-                            ),
-                            child: const Text(
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 0, 0, 0),
-                                    fontSize: 10),
-                                "Video"),
-                            onPressed: () => {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const VideoDesp(),
-                                  )),
-                            },
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 125.0,
-                        width: 125.0,
-                        child: InkWell(
-                          onTap: () {
-                            // Acción al tocar la imagen
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const VideoDesp()),
-                            );
-                          },
-                          child: Image.asset('assets/video.png'),
-                        ),
-                      ),
-                    ],
+                  CardSelect(
+                    text: 'Video de ejemplo',
+                    type: 'video',
+                    url: 'https://www.youtube.com',
                   ),
-                  //Row 2 - Informacion 1 -
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          const SizedBox(
-                            width: 180,
-                            height: 80,
-                            child: Text(
-                                style: TextStyle(fontSize: 10),
-                                " ¿Cómo dejar de procrastrinar? "),
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromARGB(255, 230, 233, 89),
-                              minimumSize: const Size(107, 21),
-                            ),
-                            child: const Text(
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 0, 0, 0),
-                                    fontSize: 10),
-                                "Información"),
-                            onPressed: () => {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const InfoDep(),
-                                  )),
-                            },
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 110.0,
-                        width: 110.0,
-                        child: InkWell(
-                          onTap: () {
-                            // Acción al tocar la imagen
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const InfoDep()),
-                            );
-                          },
-                          child: Image.asset('assets/info.png'),
-                        ),
-                      ),
-                    ],
+                  CardSelect(
+                    text: 'Como dejar de procastinar',
+                    type: 'informacion',
+                    url: 'https://x.com/',
                   ),
-                  //Row 3
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          const SizedBox(
-                            width: 180,
-                            height: 80,
-                            child: Text(
-                                style: TextStyle(fontSize: 10),
-                                " ¿Cómo sobrellevar el estés o la sobrecarga de trabajo? "),
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromARGB(255, 0, 150, 40),
-                              minimumSize: const Size(107, 21),
-                            ),
-                            child: const Text(
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 0, 0, 0),
-                                    fontSize: 10),
-                                "Video"),
-                            onPressed: () => {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const VideoDesp(),
-                                  )),
-                            },
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 125.0,
-                        width: 125.0,
-                        child: InkWell(
-                          onTap: () {
-                            // Acción al tocar la imagen
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const VideoDesp()),
-                            );
-                          },
-                          child: Image.asset('assets/video.png'),
-                        ),
-                      ),
-                    ],
+                  CardSelect(
+                    text: 'Como dejar de procastinar',
+                    type: 'video',
+                    url: 'https://x.com/',
                   ),
-                  //Row 4
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          const SizedBox(
-                            width: 180,
-                            height: 80,
-                            child: Text(
-                                style: TextStyle(fontSize: 10),
-                                " ¿Qué es la autoconciencia? "),
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromARGB(255, 230, 233, 89),
-                              minimumSize: const Size(107, 21),
-                            ),
-                            child: const Text(
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 0, 0, 0),
-                                    fontSize: 10),
-                                "Información"),
-                            onPressed: () => {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const InfoDep(),
-                                  )),
-                            },
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 110.0,
-                        width: 110.0,
-                        child: InkWell(
-                          onTap: () {
-                            // Acción al tocar la imagen
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const InfoDep()),
-                            );
-                          },
-                          child: Image.asset('assets/info.png'),
-                        ),
-                      ),
-                    ],
+                  CardSelect(
+                    text: 'Como dejar de procastinar',
+                    type: 'test',
+                    url: '',
                   ),
                 ],
               ),
---
+++ lib/widget/card.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class CardSelect extends StatelessWidget {
+  final String text;
+  final double width;
+  final double height;
+  final String type;
+  final String url;
+
+  const CardSelect({
+    Key? key,
+    required this.text,
+    required this.type,
+    required this.url,
+    this.width = 350,
+    this.height = 110,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    IconData icon;
+    String buttonText;
+    Color disabledButtonColor;
+
+    switch (type) {
+      case 'video':
+        icon = Icons.play_circle_fill;
+        buttonText = "Video";
+        disabledButtonColor = Colors.teal[200]!;
+        break;
+      case 'informacion':
+        icon = Icons.info;
+        buttonText = "Información";
+        disabledButtonColor = Colors.blue[200]!;
+        break;
+      case 'test':
+        icon = Icons.check_circle;
+        buttonText = "Test";
+        disabledButtonColor = Colors.green[200]!;
+        break;
+      default:
+        icon = Icons.help;
+        buttonText = "Abrir";
+        disabledButtonColor = Colors.grey[400]!;
+        break;
+    }
+
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      color: Colors.teal,
+      child: InkWell(
+        splashColor: const Color.fromARGB(255, 24, 109, 86),
+        onTap: () async {
+          final uri = Uri.parse(url);
+          print(uri);
+          print(url);
+          if (await canLaunchUrl(uri)) {
+            await launchUrl(uri);
+          } else {
+            throw 'Could not launch $url';
+          }
+        },
+        child: SizedBox(
+          width: width,
+          height: height,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        text,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 10,
+                        ),
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(
+                          height: 5), // Espacio entre el texto y el botón
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: disabledButtonColor,
+                          minimumSize: const Size(107, 21),
+                          disabledBackgroundColor: disabledButtonColor,
+                        ),
+                        child: Text(
+                          buttonText,
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontSize: 10,
+                          ),
+                        ),
+                        onPressed: null, // El botón está deshabilitado
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  height: 90.0,
+                  width: 80.0,
+                  child: Icon(icon, size: 50, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
