f667a327c014dd07beeaf4a7ba674d2fdac55ee3
DeimHT <DieterDDHT@gmail.com>
2024-06-03 02:35:43 -0400

    Interfaz Configuraciones
    Se terminan las interfaces de Paleta de Colores y de Personalizar Asistente

--- lib/screens/perfil/configuracion/paletaColores.dart
+++ lib/screens/perfil/configuracion/paletaColores.dart
@@ -14,7 +14,7 @@ class PaletaColores extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
-                margin: const EdgeInsets.only(bottom: 40, top: 50),
+                margin: const EdgeInsets.only(bottom: 50, top: 50),
                 height: 180,
                 width: 450,
                 child: Image.asset(
@@ -22,27 +22,28 @@ class PaletaColores extends StatelessWidget {
                 ),
               ),
               Container(
-                margin: const EdgeInsets.only(bottom: 20.0),
+                margin: const EdgeInsets.only(bottom: 100.0),
                 width: 320,
                 child: const Text("Cambiar paleta de colores", 
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 30,
                     color: Color.fromARGB(255, 23, 56, 84)
                   ),
                 ),
               ),
               Container(
-                margin: const EdgeInsets.only(bottom: 20.0),
+                margin: const EdgeInsets.only(bottom: 120.0),
                 width: 320,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Container(
+                      margin: EdgeInsets.only(right: 30),
                       child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           backgroundColor: const Color.fromARGB(255, 45, 149, 150),
-                          minimumSize: const Size(294, 50),
+                          minimumSize: const Size(50, 50),
                         ),
                         child: const Text(
                             style: TextStyle(
@@ -57,7 +58,7 @@ class PaletaColores extends StatelessWidget {
                       child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           backgroundColor: const Color.fromARGB(255, 219, 192, 95),
-                          minimumSize: const Size(294, 50),
+                          minimumSize: const Size(50, 50),
                         ),
                         child: const Text(
                             style: TextStyle(
@@ -72,29 +73,12 @@ class PaletaColores extends StatelessWidget {
                 )
               ),
               Container(
-                margin: const EdgeInsets.only(bottom: 20.0),
-                width: 320,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 38, 80, 115),
-                    minimumSize: const Size(294, 50),
-                  ),
-                  child: const Text(
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 236, 244, 214),
-                          fontSize: 20),
-                      "Cambiar paleta de colores",
-                      textAlign: TextAlign.center),
-                  onPressed: () => {},
-                ),
-              ),
-              Container(
                 margin: const EdgeInsets.only(bottom: 15.0),
-                width: 320,
+                width: 300,
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color.fromARGB(255, 179, 19, 18),
-                    minimumSize: const Size(294, 50),
+                    minimumSize: const Size(240, 50),
                   ),
                   child: const Text(
                       style: TextStyle(
--- lib/screens/perfil/configuracion/persoAsistente.dart
+++ lib/screens/perfil/configuracion/persoAsistente.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class PersoAsistente extends StatelessWidget {
-  const PersoAsistente({super.key});
+class PersoAsistente extends StatefulWidget {
+  const PersoAsistente({Key? key}) : super(key: key);
+
+  @override
+  _PersoAsistenteState createState() => _PersoAsistenteState();
+}
+
+class _PersoAsistenteState extends State<PersoAsistente>{
+
+  final _nombre = TextEditingController();
+  String asistenteSeleccionado = 'Ninguno seleccionado';
 
   @override
   Widget build(BuildContext context) {
@@ -27,55 +36,248 @@ class PersoAsistente extends StatelessWidget {
                 child: const Text("Personalizar asistente", 
                   textAlign: TextAlign.center,
                   style: TextStyle(
+                    fontSize: 30,
+                    color: Color.fromARGB(255, 23, 56, 84)
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 20.0),
+                width: 320,
+                child: Text("Asistente seleccionado:", 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
                     fontSize: 20,
                     color: Color.fromARGB(255, 23, 56, 84)
                   ),
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 20.0),
-                    width: 320,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 38, 80, 115),
-                        minimumSize: const Size(294, 50),
-                      ),
-                      child: const Text(
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 236, 244, 214),
-                              fontSize: 20),
-                          "Guardar",
-                          textAlign: TextAlign.center),
-                      onPressed: () => {},
+              Container(
+                margin: EdgeInsets.only(top: 20, bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 130,
+                      child: MyRadioMenuButton(
+                        value: 'Nutria',
+                        groupValue: asistenteSeleccionado,
+                        onChanged: (selectedValue) {
+                          setState(() => asistenteSeleccionado = selectedValue!);
+                        },
+                        style: ButtonStyle(
+                          shape: WidgetStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          elevation: const WidgetStatePropertyAll(2),
+                          backgroundColor: WidgetStatePropertyAll(Colors.teal),
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SizedBox(
+                              height: 86.0,
+                              child: Image.asset('assets/mascota.png'),
+                            ),
+                            const Text(
+                              'Nutria',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 236, 244, 214),
+                                fontSize: 20,
+                              ),
+                            ),
+                            Radio<String>(
+                              value: 'Nutria',
+                              groupValue: asistenteSeleccionado,
+                              onChanged: (selectedValue) {
+                                setState(() => asistenteSeleccionado = selectedValue!);
+                              },
+                              activeColor: Color.fromARGB(255, 236, 244, 214),
+                              fillColor: WidgetStatePropertyAll(Color.fromARGB(255, 236, 244, 214)),
+                            ),
+                          ],
+                        ),
+                      )
                     ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 15.0),
-                    width: 320,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 179, 19, 18),
-                        minimumSize: const Size(294, 50),
-                      ),
-                      child: const Text(
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 236, 244, 214),
-                              fontSize: 20),
-                          "Volver"),
-                      onPressed: () => {
-                        Navigator.pop(context)
-                      },
+                    SizedBox(
+                      width: 130,
+                      child: MyRadioMenuButton(
+                        value: 'Elefante',
+                        groupValue: asistenteSeleccionado,
+                        onChanged: (selectedValue) {
+                          setState(() => asistenteSeleccionado = selectedValue!);
+                        },
+                        style: ButtonStyle(
+                          shape: WidgetStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          elevation: const WidgetStatePropertyAll(2),
+                          backgroundColor: WidgetStatePropertyAll(Colors.teal),
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SizedBox(
+                              height: 86.0,
+                              child: Image.asset('assets/mascota.png'),
+                            ),
+                            const Text(
+                              'Elefante',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 236, 244, 214),
+                                fontSize: 20,
+                              ),
+                            ),
+                            Radio<String>(
+                              value: 'Elefante',
+                              groupValue: asistenteSeleccionado,
+                              onChanged: (selectedValue) {
+                                setState(() => asistenteSeleccionado = selectedValue!);
+                              },
+                              activeColor: Color.fromARGB(255, 236, 244, 214),
+                              fillColor: WidgetStatePropertyAll(Color.fromARGB(255, 236, 244, 214)),
+                            ),
+                          ],
+                        ),
+                      )
                     ),
+                    SizedBox(
+                      width: 130,
+                      child: MyRadioMenuButton(
+                        value: 'Panda',
+                        groupValue: asistenteSeleccionado,
+                        onChanged: (selectedValue) {
+                          setState(() => asistenteSeleccionado = selectedValue!);
+                        },
+                        style: ButtonStyle(
+                          shape: WidgetStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          elevation: const WidgetStatePropertyAll(2),
+                          backgroundColor: WidgetStatePropertyAll(Colors.teal),
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SizedBox(
+                              height: 86.0,
+                              child: Image.asset('assets/mascota.png'),
+                            ),
+                            const Text(
+                              'Panda',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 236, 244, 214),
+                                fontSize: 20,
+                              ),
+                            ),
+                            Radio<String>(
+                              value: 'Panda',
+                              groupValue: asistenteSeleccionado,
+                              onChanged: (selectedValue) {
+                                setState(() => asistenteSeleccionado = selectedValue!);
+                              },
+                              activeColor: Color.fromARGB(255, 236, 244, 214),
+                              fillColor: WidgetStatePropertyAll(Color.fromARGB(255, 236, 244, 214)),
+                            ),
+                          ],
+                        ),
+                      )
+                    )
+                  ]
+                )
+              ),
+              SizedBox(
+                height: 60,
+                width: 294,
+                child: TextField(
+                  controller: _nombre,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    fillColor: Color(0xFFECF4D6),
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    hintText: 'Nombre',
                   ),
-                ]
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 15.0),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 179, 19, 18),
+                          minimumSize: const Size(50, 50),
+                        ),
+                        child: const Text(
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 236, 244, 214),
+                                fontSize: 20),
+                            "Volver"),
+                        onPressed: () => {
+                          Navigator.pop(context)
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20.0),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 38, 80, 115),
+                          minimumSize: const Size(50, 50),
+                        ),
+                        child: const Text(
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 236, 244, 214),
+                                fontSize: 20),
+                            "Guardar",
+                            textAlign: TextAlign.center),
+                        onPressed: () => {},
+                      ),
+                    ),
+                  ]
+                ),
               ),
             ]
           ),
         ),
       ),
+    );
+  }
+}
+
+class MyRadioMenuButton extends StatelessWidget {
+  final String value;
+  final String groupValue;
+  final ValueChanged<String?> onChanged;
+  final ButtonStyle style;
+  final Widget child;
+
+  const MyRadioMenuButton({
+    Key? key,
+    required this.value,
+    required this.groupValue,
+    required this.onChanged,
+    required this.style,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: style,
+      onPressed: () => onChanged(value),
+      child: child,
     );
   }
 }
